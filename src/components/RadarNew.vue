17cc1f1f5de9c47ae04c4c391656c7c88b19daab
Andrew <andrewmgunn26@gmail.com>
2022-04-25 14:04:06 -0500

    VM14544:1 XHR finished loading: GET didnt errorr - so we took a step forward with this

--- src/components/RadarNew.vue
+++ src/components/RadarNew.vue
@@ -106,22 +106,122 @@ export default {
       }
     }
   },
+  // methods: {
+  //   query() {
+  //     if (this.queryString) {
+  //
+  //       const url = 'https://itunes.apple.com/search?'
+  //       // const li = 20;
+  //
+  //
+  //
+  //       const config = {
+  //         params: {
+  //           term: this.queryString,
+  //           // limit: this.limit,
+  //           media: this.select,
+  //           limit: this.termLimit,
+  //           // termLimit = this.termLimit
+  //         }
+  //       }
+  //
+  //       if (config.params.limit <= 10) {
+  //         config.params.limit = 11;
+  //       }
+  //
+  //       axios.get(url, config)
+  //           .then(response => {
+  //             if (response.data.results.length > 0) {
+  //               this.queryResults = response.data.results;
+  //               console.log(this.queryResults)
+  //
+  //             } else {
+  //               this.queryResults = [];
+  //
+  //               console.error('No results', this.queryResults);
+  //
+  //             }
+  //
+  //             this.$emit('finished', this.queryResults);
+  //
+  //           })
+  //           .catch(error => {
+  //             console.log('AJAX SEARCH ERROR', error);
+  //
+  //           })
+  //     }
+  //   }
+  // },
+  methods: {
+    query() {
+      // if (this.queryString) {
+
+        const url = 'https://api.fitbit.com/1.2/user/-/sleep/list.json?beforeDate=2022-04-27&sort=desc&offset=0&limit=100'
+        // const li = 20;
+        let access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhGSzMiLCJzdWIiOiI5NDNITkYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNjUwOTMwNTA1LCJpYXQiOjE2NTA4NDQxMDl9._eQFdJ-n0o9L5_j7KyKYqI6kY243AP_WZMpanEgqfG4"
+
+
+
+        const config = {
+          params: {
+            bearer:access_token
+            // term: this.queryString,
+            //authorization: access_token,
+          },
+          headers: {
+            Authorization: `Bearer ${access_token}`
+          }
+        }
+
+        // if (config.params.limit <= 10) {
+        //   config.params.limit = 11;
+        // }
+
+        axios.get()
+
+        axios.get(url, config,)
+            .then(response => {
+              if (response.data.results.length > 0) {
+                this.queryResults = response.data.results;
+                console.log(this.queryResults)
+                this.chartData = response.json;
+                this.loaded = true
+
+              } else {
+                this.queryResults = [];
+
+                console.error('No results', this.queryResults);
+
+              }
+
+              this.$emit('finished', this.queryResults);
+
+            })
+            .catch(error => {
+              console.log('AJAX SEARCH ERROR', error);
+
+            })
+      }
+    // }
+  },
+
   async mounted() {
-    this.loaded = false
-    const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhGSzMiLCJzdWIiOiI5NDNITkYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNjUwOTMwNTA1LCJpYXQiOjE2NTA4NDQxMDl9._eQFdJ-n0o9L5_j7KyKYqI6kY243AP_WZMpanEgqfG4"
-    try {
-      let response = fetch('https://api.fitbit.com/1.2/user/-/sleep/list.json?beforeDate=2022-04-27&sort=desc&offset=0&limit=100', {
-        method: "GET",
-        headers: {"Authorization": "Bearer " + access_token}
-      })
-          .then(response => response.json())
-          .then(json => json.sleep);
-      this.chartData = response.json;
-      this.loaded = true
-      console.log(response.json)
-    } catch (e) {
-      console.error(e)
-    }
+    // this.loaded = false
+    // const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhGSzMiLCJzdWIiOiI5NDNITkYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNjUwOTMwNTA1LCJpYXQiOjE2NTA4NDQxMDl9._eQFdJ-n0o9L5_j7KyKYqI6kY243AP_WZMpanEgqfG4"
+    // try {
+    //   let response = fetch('https://api.fitbit.com/1.2/user/-/sleep/list.json?beforeDate=2022-04-27&sort=desc&offset=0&limit=100', {
+    //     method: "GET",
+    //     headers: {"Authorization": "Bearer " + access_token}
+    //   })
+    //       .then(response => response.json())
+    //       .then(json => json.sleep);
+    //   this.chartData = response.json;
+    //   this.loaded = true
+    //   console.log(response.json)
+    // } catch (e) {
+    //   console.error(e)
+    // }
+    this.query();
   },
 }
 </script>

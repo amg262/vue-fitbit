d19e7e99de8cd001fbfb6e6693f352806989ec41
Andrew <andrewmgunn26@gmail.com>
2022-05-02 13:36:03 -0500

    stuff

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+
 // router setup
 //import router from './ro?utes/router';
 // plugin setup

26c7c0b3d3f6858a0018ce2ab284605dd19835b9
Andrew <andrewmgunn26@gmail.com>
2022-05-04 19:52:59 -0500

    refreshed API key

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <BarChart
-        identity="https://api.fitbit.com/1.2/user/-/sleep/list.json?beforeDate=2022-04-27&sort=desc&offset=0&limit=25"></BarChart>
+        identity="https://api.fitbit.com/1.2/user/-/sleep/list.json?beforeDate=2022-04-27&sort=desc&offset=0&limit=100"></BarChart>
   </div>
 
 </template>

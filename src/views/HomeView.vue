7f151b8c5cb40c3c915c7a51e1466a7d307f7096
Andrew <andrewmgunn26@gmail.com>
2022-05-09 08:51:22 -0500

    home page done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,183 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <PieChart
-        identity="https://api.fitbit.com/1/user/-/devices.json"
-        method-name="deviceDough"
-    >
+  <div>
 
-    </PieChart>
+    <div class="row home m-5">
+      <div class="align-items-md-center">
+        <img alt="Vue logo" src="../assets/logo.png" width="200" height="200">
+        <img src="../assets/fitbit.png" alt="fitbit" width="200" height="200"/>
+
+
+      </div>
+      <!--    <PieChart-->
+      <!--        identity="https://api.fitbit.com/1/user/-/devices.json"-->
+      <!--        method-name="deviceDough"-->
+      <!--    >-->
+
+      <!--    </PieChart>-->
+    </div>
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-md-6" style="text-align: justify;">
+        <h1 style="text-align: center;">Vue Fitbit</h1>
+        <p>
+          Web application built on Vue.js v2, Bootstrap, Bootstrap-vue, AJAX, Chart.js and the Fitbit SDK & API.
+        </p>
+        <p>
+          The core functionality
+          of this application is to retrieve health related data from a Fitbit users smartwatch, tracker, and/or scale
+          and chart that data into intuitive and interactive charts and graphs in real time.
+        </p>
+      </div>
+      <div class="col"></div>
+    </div>
+
+    <div class="row m-5">
+
+      <div class="col-md-4 mt-3 mb-3">
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <img src="../assets/body.gif" alt="Image" class="rounded-0" height="240" width="240" />
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Body">
+                  <b-card-text>
+                    <p>
+                      The Body endpoints are used for querying and modifying the user's body fat and weight data.
+                    </p>
+                    <button class="btn btn-primary">
+                      <router-link class="fb-card" style="color:white;text-decoration: none;" to="/body">
+                        Body
+                      </router-link>
+                    </button>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+      </div>
+
+      <div class="col-md-4 mt-3 mb-3">
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <img src="../assets/device2.jpg" alt="Image" class="rounded-0" height="250" width="250" />
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Devices">
+                  <b-card-text>
+                    <p>
+                      The Devices endpoints displays information about the devices paired to a user's account.
+                    </p>
+                    <button class="btn btn-primary">
+                      <router-link class="fb-card" style="color:white;text-decoration: none;" to="/devices">
+                        Devices
+                      </router-link>
+                    </button>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+      </div>
+
+      <div class="col-md-4 mt-3 mb-3">
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <img src="../assets/heart.jpg" alt="Image" class="rounded-0" height="250" />
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Heart Rate">
+                  <b-card-text>
+                    <p>
+                      The Heart Rate Time Series endpoints are used for querying the user's heart rate data.
+                    </p>
+                    <button class="card-btn btn btn-outline-primary">
+                      <router-link class="fb-card" style="text-decoration: none;" to="/devices">
+                        Heart Rate
+                      </router-link>
+                    </button>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+      </div>
+
+      <div class="col-md-4 mt-3 mb-3">
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <img src="../assets/sleep.jpg" alt="Image" class="rounded-0" height="260" width="260" />
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Sleep">
+                  <b-card-text>
+                    <p>
+                      The Sleep endpoints are used for querying and modifying the sleep data.
+                    </p>
+                    <button class="card-btn btn btn-outline-primary">
+                      <router-link class="fb-card" style="text-decoration: none;" to="/sleep">
+                        Sleep
+                      </router-link>
+                    </button>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+      </div>
+
+      <div class="col-md-4 mt-3 mb-3">
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+            <b-row no-gutters>
+              <b-col md="6">
+                <img src="../assets/user.png" alt="Image" class="rounded-0" height="240" width="240" />
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="User">
+                  <b-card-text>
+                    <p>
+                      The User endpoints display information about the user's profile information and their badges collected.
+                    </p>
+                    <button class="card-btn btn btn-outline-primary">
+                      <router-link class="fb-card" style="text-decoration: none;" to="/user">
+                        User
+                      </router-link>
+                    </button>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+      </div>
+
+
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import PieChart from "@/components/Pie";
+// import PieChart from "@/components/Pie";
 
 export default {
   name: 'HomeView',
   components: {
-    PieChart
+    // PieChart
   }
 }
 </script>
